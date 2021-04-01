4e979e60473d4683f4535341ce7928536d9a92b7
MapGiser <937061883@qq.com>
2021-04-02 00:34:13 +0800

    格式化

--- src/js/planMode.js
+++ src/js/planMode.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-31 10:34:28
+ * @LastEditTime: 2021-04-01 23:26:58
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \derive\src\js\planMode.js
+ */
 let planMode = {
   fire: 0,//火
   fireworks: 1, //烟
