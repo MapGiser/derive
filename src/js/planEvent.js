390291a74a10e2212d5a90a074ce1723f9caac6e
MapGiser <937061883@qq.com>
2021-04-14 00:52:35 +0800

    bug修改

--- src/js/planEvent.js
+++ src/js/planEvent.js
@@ -93,8 +93,8 @@ PlanEvent.prototype.addFireEvent = function () {
         // that._event.startColor = that._event.endColornew = new Cesium.Color(0, 0, 0, 0.0);
       } else if (Cesium.JulianDate.lessThan(time, that._startTime)) {
         // that._event.startColor = new Cesium.Color(0, 0, 0, 0.0);
-        that._event.image = that._image;
-        that._event.startScale = that._event.endScale = 1.0;
+        // that._event.image = that._image;
+        // that._event.startScale = that._event.endScale = 1.0;
       } else if (Cesium.JulianDate.greaterThan(time, that._startTime) && Cesium.JulianDate.lessThan(time, middleTime)) {
         that._event.emissionRate = that._emissionRate || 180;
         that._event.startScale = startScale;
@@ -116,7 +116,7 @@ PlanEvent.prototype.addFireEvent = function () {
     return new Cesium.ParticleSystem({
       modelMatrix: modelMatrix,
       speed: this._speed || 15,
-      lifetime: this._lifetime || 0.8,
+      lifetime: 500,
       particleLife: 0.5,
       show: true,
       startScale: 1,
@@ -134,196 +134,92 @@ PlanEvent.prototype.addFireEvent = function () {
   }
 }
 
-PlanEvent.prototype.addWaterEvent = function () {
-  let positionOringon = this._positionOringon;//pos1
-  let positionEnd = this._positionEnd;//pos3
-  if (!positionOringon || !positionEnd) return;
-  let offsetHeight = this._offsetHeight || 3.4;
-  let image = this._image || './static/data/img/pq.png';
-  let emissionRate = this._emissionRate || 50;
-  let startScale = this._startScale || 1;
-  let endScale = this._endScale || 1;
-  let gravity = -1;
-  var angle = this.computerAzimuth(positionOringon, positionEnd);//方位角
-  var gravityScratch = new Cesium.Cartesian3();
-
-  //计算距离
-  var distance = Cesium.Cartesian3.distance(positionOringon, positionEnd);
-  var minimumParticleLife = distance / 2;
-  var maximumParticleLife = distance / 2;
-  var speedMin, speedMax, pitch;
-  //根据距离计算射程速度，决定能否射入目标点
-  if (distance < 10) {
-    speedMin = 2.0;
-    speedMax = 2.5;
-    pitch = Math.PI / 3;
-    startScale = 0.6;
-    endScale = 0.5;
-    minimumParticleLife = maximumParticleLife = 15.0;
-    emissionRate = 20.0;
-  } else if (distance < 20) {
-    speedMin = 3.0;
-    speedMax = 4.0;
-    pitch = Math.PI / 3;
-    endScale = 1.2;
-  } else if (distance < 30) {
-    speedMin = 1 * Math.pow(2.1, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(2.2, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 1.8;
-  } else if (distance < 40) {
-    speedMin = 1 * Math.pow(2.3, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(2.4, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 1.8;
-  } else if (distance < 50) {
-    speedMin = 1 * Math.pow(2.4, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(2.5, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 2.0;
-  } else if (distance < 60) {
-    speedMin = 1 * Math.pow(2.6, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(2.7, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 2.0;
-  } else if (distance < 70) {
-    speedMin = 1 * Math.pow(2.7, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(2.85, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 2.0;
-  } else if (distance < 80) {
-    speedMin = 1 * Math.pow(2.8, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(2.9, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 2.0;
-  } else if (distance < 100) {
-    speedMin = 1 * Math.pow(3.0, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.1, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 2.0;
-  } else if (distance < 120) {
-    speedMin = 1 * Math.pow(3.1, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.2, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 2.0;
-  } else if (distance < 140) {
-    speedMin = 1 * Math.pow(3.2, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.35, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 2.0;
-  } else if (distance < 160) {
-    speedMin = 1 * Math.pow(3.3, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.4, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 3.0;
-  } else if (distance < 170) {
-    speedMin = 1 * Math.pow(3.45, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.55, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 3.0;
-  } else if (distance < 180) {
-    speedMin = 1 * Math.pow(3.55, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.65, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 4.0;
-  } else if (distance < 190) {
-    speedMin = 1 * Math.pow(3.60, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.70, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 4.0;
-  } else if (distance < 200) {
-    speedMin = 1 * Math.pow(3.6, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.7, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 4.0;
-  } else if (distance < 220) {
-    speedMin = 1 * Math.pow(3.7, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.8, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 4.0;
-  } else if (distance < 240) {
-    speedMin = 1 * Math.pow(3.8, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(3.9, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 4.0;
-  } else if (distance < 260) {
-    speedMin = 1 * Math.pow(3.9, parseInt(distance / minimumParticleLife));
-    speedMax = 1 * Math.pow(4.0, parseInt(distance / minimumParticleLife)) + 0.5;
-    pitch = Math.PI / 4;
-    endScale = 4.0;
-  }
+PlanEvent.prototype.addFfireWorksEvent = function (options) {
+  let position = this._position;
 
   var emitterModelMatrix = new Cesium.Matrix4();
   var translation = new Cesium.Cartesian3();
   var rotation = new Cesium.Quaternion();
-  var hpr = new Cesium.HeadingPitchRoll();
-  var hpr = new Cesium.HeadingPitchRoll(angle, pitch, 0);
+  // var hpr = new Cesium.HeadingPitchRoll();
+  var hpr = new Cesium.HeadingPitchRoll(this._fireWorksBearAngle, this._fireWorksLevelAngle, 0);
   var trs = new Cesium.TranslationRotationScale();
+
+  let startScale = 2.0;
+  let endScale = 3.0;
 
   function computeEmitterModelMatrix() {
     //喷泉位置
-    trs.translation = Cesium.Cartesian3.fromElements(0, 0, offsetHeight, translation);
+    trs.translation = Cesium.Cartesian3.fromElements(0, 0, 0.1, translation);
     trs.rotation = Cesium.Quaternion.fromHeadingPitchRoll(hpr, rotation);
 
     return Cesium.Matrix4.fromTranslationRotationScale(trs, emitterModelMatrix);
   }
-  var that = this;
-  //设置重力
-  function applyGravity(p, dt) {
-    var position = p.position;
-    Cesium.Cartesian3.normalize(position, gravityScratch);
-    Cesium.Cartesian3.multiplyByScalar(gravityScratch, gravity * dt, gravityScratch);
-    p.velocity = Cesium.Cartesian3.add(p.velocity, gravityScratch, p.velocity);
 
-    let time = that._viewer.clock.currentTime;
-    if (Cesium.JulianDate.lessThan(time, that._startTime)) {
-      that._event.startColor = new Cesium.Color(0, 0, 0, 0.0);
-      that._event.image = null;
+  let that = this;
+  function applyGravity(particle, dt) {
+    var times = Cesium.JulianDate.secondsDifference(that._endTime, that._startTime);
+    var middleTime = Cesium.JulianDate.addSeconds(that._startTime, times / 2, new Cesium.JulianDate());
+    if (particle) {
+      let time = that._viewer.clock.currentTime;
+      if (Cesium.JulianDate.greaterThan(time, that._endTime)) {
+        that._event.startScale = that._event.endScale = 0.01;
+        // that._event.image = that._image;
+        // that._event.startColor = that._event.endColornew = new Cesium.Color(0, 0, 0, 0.0);
+      } else if (Cesium.JulianDate.lessThan(time, that._startTime)) {
+        // that._event.startColor = new Cesium.Color(0, 0, 0, 0.0);
+        // that._event.image = null;
+        // that._event.startScale = that._event.endScale = 0.01;
+      } else if (Cesium.JulianDate.greaterThan(time, that._startTime) && Cesium.JulianDate.lessThan(time, that._endTime)) {
+        // that._event.emissionRate = that._emissionRate || 180;
+        that._event.startScale = startScale;
+        // that._event.image = that._image;
+        that._event.endScale = endScale;
+      }
+      else if (Cesium.JulianDate.greaterThan(time, middleTime) && Cesium.JulianDate.lessThan(time, that._endTime)) {
+        // that._event.startScale = startScale;
+        // that._event.image = that._image;
+        // that._event.endScale = endScale;
 
-      that._event.startScale = that._event.endScale = 0.01;
-      // that._event.show = false;
-    } else if (Cesium.JulianDate.greaterThan(time, that._startTime) && Cesium.JulianDate.lessThan(time, that._endTime)) {
-      // that._event.emissionRate = that._emissionRate;
-      // that._event.show = true;
-      that._event.startScale = startScale
-      that._event.image = image;
-      that._event.startColor = new Cesium.Color(1, 1, 1, 0.6);
-      that._event.endColor = new Cesium.Color(0.80, 0.86, 1, 0.4)
-      that._event.endScale = endScale;
-    } else if (Cesium.JulianDate.greaterThan(time, that._endTime)) {
-      // that._event.emissionRate = 0.001;
-      // that._event.show = false;
-      that._event.startScale = that._event.endScale = 0.01;
-      that._event.image = null;
-      that._event.startColor = that._event.endColornew = new Cesium.Color(0, 0, 0, 0.0);
+        // let emissionRate = ((that._endTime.secondsOfDay - time.secondsOfDay) / (that._endTime.secondsOfDay - middleTime.secondsOfDay))
+        // if (emissionRate < 0.1) emissionRate = 0.01;
+        // that._event.emissionRate = emissionRate * that._emissionRate;
+      }
     }
   }
 
-
-  //添加粒子
-  return new Cesium.ParticleSystem({
-    image: image,
-    startColor: new Cesium.Color(1, 1, 1, 0.6),
-    endColor: new Cesium.Color(0.80, 0.86, 1, 0.4),
-    startScale: 0.01,
-    endScale: 0.01,
-    show: true,
-    minimumParticleLife: minimumParticleLife,
-    maximumParticleLife: maximumParticleLife,
-    minimumSpeed: speedMin,
-    maximumSpeed: speedMax,
-    imageSize: new Cesium.Cartesian2(3.6, 3.6),
-    emissionRate: emissionRate,
-    emitter: new Cesium.CircleEmitter(0.5),
-    modelMatrix: Cesium.Transforms.eastNorthUpToFixedFrame(positionOringon),//computeModelMatrix(entity, viewer.clock.currentTime),
-    emitterModelMatrix: computeEmitterModelMatrix(),
-    updateCallback: this._updateCallback || applyGravity,
-    sizeInMeters: true
-  })
+  if (position) {
+    let modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(position);
+    return new Cesium.ParticleSystem({
+      image: './static/data/img/smoke.png',
+      modelMatrix: modelMatrix,
+      show: true,
+      startColor: options.startColor || Cesium.Color.fromCssColorString('#c98d33').withAlpha(0.5),
+      endColor: options.endColor || Cesium.Color.BLACK.withAlpha(0.15),
+      startScale: options.startScale || 2.0,
+      endScale: options.endScale || 3.0,
+      // minimumParticleLife: options.minimumParticleLife || 1.2,
+      // maximumParticleLife: options.maximumParticleLife || 3.1,
+      minimumSpeed: options.minimumSpeed || 3.0,
+      maximumSpeed: options.maximumSpeed || 5.0,
+      imageSize: options.imageSize || new Cesium.Cartesian2(2.0, 2.0),
+      emissionRate: options.emissionRate || 3.0,
+      // bursts: [
+      //   // these burst will occasionally sync to create a multicolored effect
+      //   new Cesium.ParticleBurst({ time: 5.0, minimum: 1, maximum: 10 }),
+      //   new Cesium.ParticleBurst({ time: 10.0, minimum: 3, maximum: 5 }),
+      //   new Cesium.ParticleBurst({ time: 15.0, minimum: 2, maximum: 1 })
+      // ],
+      lifetime: 25.0,
+      emitter: new Cesium.CircleEmitter(1.0),
+      sizeInMeters: true,
+      emitterModelMatrix: computeEmitterModelMatrix(),
+      updateCallback: this._updateCallback || applyGravity,
+    })
+  }
 
 }
 
-PlanEvent.prototype.addWaterEvent1 = function () {
+PlanEvent.prototype.addWaterEvent = function () {
   let positionOringon = this._positionOringon.clone();//pos1
   let positionEnd = this._positionEnd.clone();//pos3
   if (!positionOringon || !positionEnd) return;
@@ -467,9 +363,9 @@ PlanEvent.prototype.addWaterEvent1 = function () {
       let time = clock.currentTime;
       if (Cesium.JulianDate.lessThan(time, that._startTime) && Cesium.JulianDate.greaterThan(time, that._minimumTime)) {
         that._event.startColor = new Cesium.Color(0, 0, 0, 0.0);
-        that._event.image = null;
+        // that._event.image = null;
 
-        that._event.startScale = that._event.endScale = 0.01;
+        // that._event.startScale = that._event.endScale = 0.01;
         // that._event.show = false;
       } else if (Cesium.JulianDate.greaterThan(time, that._startTime) && Cesium.JulianDate.lessThan(time, that._endTime)) {
         // that._event.emissionRate = that._emissionRate;
@@ -499,6 +395,7 @@ PlanEvent.prototype.addWaterEvent1 = function () {
     endScale: 0.01,
     show: true,
     minimumParticleLife: minimumParticleLife,
+    lifetime: minimumParticleLife + maximumParticleLife,
     maximumParticleLife: maximumParticleLife,
     minimumSpeed: speedMin,
     maximumSpeed: speedMax,
@@ -508,18 +405,81 @@ PlanEvent.prototype.addWaterEvent1 = function () {
     modelMatrix: Cesium.Transforms.eastNorthUpToFixedFrame(positionOringon),//computeModelMatrix(entity, viewer.clock.currentTime),
     emitterModelMatrix: computeEmitterModelMatrix(),
     updateCallback: applyGravitys,
-    // sizeInMeters: true,
-    mass: this._mass || 300,
-    sizeInMeters: this._sizeInMeters || true,
-    loop: true
+    sizeInMeters: true,
+    // mass: this._mass || 300,
+    // sizeInMeters: this._sizeInMeters || true,
+    // loop: true
   })
 
   return event;
 
 }
 
+PlanEvent.prototype.addExplodeEvent = function (options) {
+  let position = this._position
+  if (position) {
+    let modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(position);
+    let emitterModelMatrixScratch = new Cesium.Matrix4();
+    //计算偏移
+    position = Cesium.Cartesian3.add(new Cesium.Cartesian3(0.0, 0.0, 0.0), new Cesium.Cartesian3(0.2, 0.1, 0.1), new Cesium.Cartesian3());
+    //计算偏移矩阵(世界坐标)
+    var emitterModelMatrix = Cesium.Matrix4.fromTranslation(position, emitterModelMatrixScratch);
+    // 粒子偏移后的矩阵（世界坐标）
+    var particleToWorld = Cesium.Matrix4.multiply(modelMatrix, emitterModelMatrix, new Cesium.Matrix4());
+    // 计算偏移坐标的逆矩阵
+    var worldToParticle = Cesium.Matrix4.inverseTransformation(particleToWorld, particleToWorld);
+    // 计算粒子大小的随机数
+    var size = Cesium.Math.randomBetween(1.6, 1.7);
+    var particlePositionScratch = new Cesium.Cartesian3();
+    //粒子大小
+    var normalSize = size;
+    var minLife = 4.3;
+    var maxLife = 5.0;
+    //计算生命周期比例  0-----1之间
+    var life = normalSize * (maxLife - minLife) + minLife;
+    // 添加粒子
+    var bursts = [];
+    bursts.push(new Cesium.ParticleBurst({
+      time: 2.2,//Cesium.Math.nextRandomNumber() * 16.0,
+      minimum: 2.0,
+      maximum: 2.0 * 2.5
+    }));
+    var force = function (particle) {
+      //根据粒子的位置计算偏移的局部坐标
+      var position = Cesium.Matrix4.multiplyByPoint(worldToParticle, particle.position, particlePositionScratch);
+      if (Cesium.Cartesian3.magnitudeSquared(position) >= size * size) {
+        Cesium.Cartesian3.clone(Cesium.Cartesian3.ZERO, particle.velocity);
+      }
+    };
+    return new Cesium.ParticleSystem({
+      startColor: options.startColor || Cesium.Color.BLACK.withAlpha(0.65),
+      endColor: options.endColor || Cesium.Color.fromCssColorString('#c98d33').withAlpha(0.15),
+      image: './static/data/img/smoke.png',
+      particleLife: life,
+      speed: 15.0,
+      show: true,
+      imageSize: new Cesium.Cartesian2(3, 4),
+      emissionRate: 0,
+      emitter: new Cesium.SphereEmitter(2.0),
+      bursts: bursts,
+      lifetime: 5.0,
+      updateCallback: force,
+      // bursts: [
+      //   // these burst will occasionally sync to create a multicolored effect
+      //   new Cesium.ParticleBurst({ time: 5.0, minimum: 1, maximum: 10 }),
+      //   new Cesium.ParticleBurst({ time: 10.0, minimum: 3, maximum: 5 }),
+      //   new Cesium.ParticleBurst({ time: 15.0, minimum: 2, maximum: 1 })
+      // ],
+      modelMatrix: modelMatrix,
+      emitterModelMatrix: emitterModelMatrix,
+      sizeInMeters: true
+    })
 
-Object.defineProperties(PlanEvent, {
+  }
+}
+
+
+Object.defineProperties(PlanEvent.prototype, {
   positionOringon: {
     get: function () {
       return this._positionOringon;
