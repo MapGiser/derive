8304bc0001ca5735f81f3794a1149b09b64af3e3
MapGiser <937061883@qq.com>
2021-04-14 10:44:29 +0800

    注释

--- src/js/planMode.js
+++ src/js/planMode.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-03-31 10:34:28
- * @LastEditTime: 2021-04-01 23:26:58
- * @LastEditors: your name
+ * @LastEditTime: 2021-04-14 10:29:53
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \derive\src\js\planMode.js
  */
@@ -10,7 +10,7 @@ let planMode = {
   fire: 0,//火
   fireworks: 1, //烟
   water: 2, // 水
-  explode: 3, // 水
+  explode: 3, // 爆炸
 }
 
 export default Object.freeze(planMode);
